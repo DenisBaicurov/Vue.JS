43a345dfbb4d5abdeea6a375bce8c5d59c023a1d
DenisBaicurov <denisbaicurov@mail.ru>
2023-11-21 20:58:14 +0400

    Сделали 7 задание

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProjectDetails />
+
+    <ProjectPage />
+
+    <HomePage />
+
+    <BlogPage />
+
+    <BlogDetailsPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HomePage from './pages/HomePage.vue'
+
+import BlogPage from './pages/BlogPage.vue'
+
+import BlogDetailsPage from './pages/BlogDetailsPage.vue'
+
+import ProjectPage from './pages/ProjectPage.vue'
+
+import ProjectDetails from './pages/ProjectDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+
+    BlogPage,
+
+    BlogDetailsPage,
+
+    ProjectPage,
+
+    ProjectDetails
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "scss/vars";
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - $widthSite / 2);
+  padding-right: calc(50% - $widthSite / 2);
 }
 </style>
